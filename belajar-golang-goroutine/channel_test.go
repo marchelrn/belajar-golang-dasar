76fb01e816423a95adb86c6141ba85ce9a0eb1c2
marchelcde <marchelmanullang026@student.unsrat.ac.id>
2025-12-28 16:02:29 +0800

    sesi siang

--- belajar-golang-goroutine/channel_test.go
+++ belajar-golang-goroutine/channel_test.go
@@ -19,9 +19,9 @@ func TestCreateChannel(t *testing.T) {
 	close(channel)
 }
 
-func Response(chann chan string) {
+func Response(channel chan string) {
 	time.Sleep(2 * time.Second)
-	chann <- "Marchel Matthew Manullang"
+	channel <- "Marchel Matthew Manullang"
 }
 
 func TestResponse(t *testing.T) {
@@ -66,4 +66,70 @@ func TestBufferedChannel(t *testing.T) {
 	fmt.Println(<-channel)
 
 	fmt.Println("Selesai")
+
+}
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- fmt.Sprintf("Data ke-%d", i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println(data)
+	}
+	fmt.Println("Selesai")
+}
+
+func TestSelectChannel(t *testing.T) {
+	channel1 := make(chan string, 2)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+	go Response(channel1)
+	go Response(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1:", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2:", data)
+			counter++
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}
+
+func TestSelectChannelDefault(t *testing.T) {
+	channel1 := make(chan string, 2)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+	go Response(channel1)
+	go Response(channel2)
+
+	counter := 0
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1:", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2:", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data...")
+		}
+		if counter == 2 {
+			break
+		}
+	}
 }
